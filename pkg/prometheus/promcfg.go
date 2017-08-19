e4f008425b750e8311e2efb64c3bebda2f3634ba
gdmelloatpoints <gavin.dmello@points.com>
2017-08-19 01:49:35 -0400

    Correctly set maps.

--- pkg/prometheus/promcfg.go
+++ pkg/prometheus/promcfg.go
@@ -125,10 +125,10 @@ func generateConfig(p *v1alpha1.Prometheus, mons map[string]*v1alpha1.ServiceMon
 	})
 
 	if len(p.Spec.RemoteWriteEndpoints) > 0 {
-		remoteWriteConfigs := make([]yaml.MapSlice, len(p.Spec.RemoteWriteEndpoints))
-		for _, url := range p.Spec.RemoteWriteEndpoints {
+		var remoteWriteConfigs []yaml.MapSlice
+		for _, rw := range p.Spec.RemoteWriteEndpoints {
 			remoteWriteConfigs = append(remoteWriteConfigs, yaml.MapSlice {
-				{ Key:   "url", Value: url},
+				{ Key:   "url", Value: rw.Url},
 			})
 		}
 
@@ -139,10 +139,10 @@ func generateConfig(p *v1alpha1.Prometheus, mons map[string]*v1alpha1.ServiceMon
 	}
 
 	if len(p.Spec.RemoteReadEndpoints) > 0 {
-		remoteReadConfigs := make([]yaml.MapSlice, len(p.Spec.RemoteReadEndpoints))
-		for _, url := range p.Spec.RemoteReadEndpoints {
+		var remoteReadConfigs []yaml.MapSlice
+		for _, rr := range p.Spec.RemoteReadEndpoints {
 			remoteReadConfigs = append(remoteReadConfigs, yaml.MapSlice {
-				{ Key:   "url", Value: url},
+				{ Key:   "url", Value: rr.Url},
 			})
 		}
 
